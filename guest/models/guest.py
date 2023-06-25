c1406430a642a308f08e9782deb82ba9393429a8
Hordii-Yurii Rushynets <hordii-yurii.rushynets@pe.lifecell.com.ua>
2023-06-25 14:24:37 +0300

    add string representaion of guest model

--- guest/models/guest.py
+++ guest/models/guest.py
@@ -6,3 +6,6 @@ class Guest(models.Model):
     """An object that describes Guests of KFC."""
     unique_id = models.CharField(unique=True, max_length=150, blank=True, null=True)
     created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.unique_id} - {self.created_at}"
